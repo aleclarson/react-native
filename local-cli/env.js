d0a7b4859ae56fe591351b909894f56d112d0692
aleclarsoniv <alec.stanford.larson@gmail.com>
2016-07-14 12:38:50 -0700

    Move 'local-cli' to its own repo (see 'aleclarson/react-native-cli')

--- local-cli/env.js
+++
@@ -1,9 +0,0 @@
-
-require('babel-polyfill');
-
-require(process.env.LOTUS_PATH + '/react-packager/env');
-
-require('babel-register/only')(
-  [ /react-native\/local-cli/ ],
-  require('path').resolve(__dirname, '..', '.babelrc')
-);
