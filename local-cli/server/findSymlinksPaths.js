8074e32019bb27909da409da322187276c4f0a79
aleclarsoniv <alec.stanford.larson@gmail.com>
2016-09-07 09:04:31 -0700

    [local-cli] Support symlinks-to-symlinks

--- local-cli/server/findSymlinksPaths.js
+++ local-cli/server/findSymlinksPaths.js
@@ -6,7 +6,12 @@ module.exports = function findSymlinksPaths(lookupFolder) {
   const folders = fs.readdirSync(lookupFolder);
   const resolvedSymlinks = folders.map(folder => path.resolve(lookupFolder, folder))
     .filter(folderPath => fs.lstatSync(folderPath).isSymbolicLink())
-    .map(symlink => path.resolve(process.cwd(), fs.readlinkSync(symlink)));
+    .map(symlink => {
+      do {
+        symlink = path.resolve(process.cwd(), fs.readlinkSync(symlink));
+      } while (fs.lstatSync(symlink).isSymbolicLink());
+      return symlink;
+    });
   const timeEnd = Date.now();
 
   console.log(`Scanning ${folders.length} folders for symlinks in ${lookupFolder} (${timeEnd - timeStart}ms)`);
