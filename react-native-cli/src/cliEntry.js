1c5637df263b07df15ba8e52861c5b6c92b0d657
aleclarson <alec.stanford.larson@gmail.com>
2016-09-17 22:59:13 -0700

    Move the setup_env bash scripts out of the src directory

--- react-native-cli/src/cliEntry.js
+++ react-native-cli/src/cliEntry.js
@@ -154,7 +154,7 @@ function run() {
     ? 'setup_env.bat'
     : 'setup_env.sh';
 
-  childProcess.execFileSync(path.join(__dirname, setupEnvScript));
+  childProcess.execFileSync(path.resolve(__dirname, '..', setupEnvScript));
 
   const config = getCliConfig();
   commands.forEach(cmd => addCommand(cmd, config));
