23997af731764d089927eda60b96f2136b7d6b39
aleclarson <alec.stanford.larson@gmail.com>
2016-09-18 19:09:34 -0700

    Disable the 'launchChromeDevTools' function

--- react-native-cli/src/server/middleware/getDevToolsMiddleware.js
+++ react-native-cli/src/server/middleware/getDevToolsMiddleware.js
@@ -26,13 +26,13 @@ function getChromeAppName() {
 }
 
 function launchChromeDevTools(port) {
-  var debuggerURL = 'http://localhost:' + port + '/debugger-ui';
-  console.log('Launching Dev Tools...');
-  opn(debuggerURL, {app: [getChromeAppName()]}, function(err) {
-    if (err) {
-      console.error('Google Chrome exited with error:', err);
-    }
-  });
+  // var debuggerURL = 'http://localhost:' + port + '/debugger-ui';
+  // console.log('Launching Dev Tools...');
+  // opn(debuggerURL, {app: [getChromeAppName()]}, function(err) {
+  //   if (err) {
+  //     console.error('Google Chrome exited with error:', err);
+  //   }
+  // });
 }
 
 function escapePath(path) {
